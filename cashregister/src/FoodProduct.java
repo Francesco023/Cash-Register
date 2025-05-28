d04f9785dd2e0d3875d35e409e6389d4e5251f81
Youssef-Branchina <youssef.abdelaziz@branchina.edu.it>
2025-05-28 10:27:35 +0200

    Commentata la classe FoodProduct.

--- cashregister/src/FoodProduct.java
+++ cashregister/src/FoodProduct.java
@@ -1,18 +1,35 @@
 package cashregister.src;
 
+/**
+ * questa è una classe derivata dalla classe product e serve a mettere i prodotti di tipo cibo specificandone le data di scadenza e la categoria
+ */
 public class FoodProduct extends Product {
     private String expirationDate;
 
+    /**
+     * questo è il metodo costruttore che usa il super per prendere i parametri dalla classe padre
+     * @param name nome del prodotto
+     * @param price prezzo del prodotto
+     * @param expirationDate data di scadenza del prodotto
+     */
     public FoodProduct(String name, double price, String expirationDate) {
         super(name, price);
         this.expirationDate = expirationDate;
     }
 
+    /**
+     * serve a ricavare la categoria
+     * @return ritorna la categoria cibo
+     */
     @Override
     public String getCategory() {
         return "Food";
     }
 
+    /**
+     * serve a ricavare la data di scadenza
+     * @return ritorna la data di scadenza
+     */
     public String getExpirationDate() {
         return expirationDate;
     }
