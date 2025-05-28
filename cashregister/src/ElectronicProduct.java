b4d2fe4d452b961e0314ffce362c6cfe621b0396
Youssef-Branchina <youssef.abdelaziz@branchina.edu.it>
2025-05-28 10:31:47 +0200

    Commentata la classe ElectronicProduct.

--- cashregister/src/ElectronicProduct.java
+++ cashregister/src/ElectronicProduct.java
@@ -1,18 +1,36 @@
 package cashregister.src;
 
+/**
+ * questa è una classe derivata per i prodotti elettronici specificandone anche i mesi di garanzia
+ */
+
 public class ElectronicProduct extends Product {
     private int warrantyMonths;
 
+    /**
+     * questo è il metodo costruttore, usa anche il comando super per prendere i parametri della classe padre
+     * @param name nome del prodotto
+     * @param price prezzo del prodotto
+     * @param warrantyMonths mesi di garanzia
+     */
     public ElectronicProduct(String name, double price, int warrantyMonths) {
         super(name, price);
         this.warrantyMonths = warrantyMonths;
     }
 
+    /**
+     * metodo per ricavare la categoria
+     * @return ritorna la categoria "Electronics"
+     */
     @Override
     public String getCategory() {
         return "Electronics";
     }
 
+    /**
+     * ricava i mesi per la garanzia
+     * @return ritorna i mesi della garanzia
+     */
     public int getWarrantyMonths() {
         return warrantyMonths;
     }
